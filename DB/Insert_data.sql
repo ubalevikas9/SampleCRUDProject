b5519202636c970ad47ef1fdcbd8a33f59efb0d3
Vikas Ubale <developer@danphehealth.com>
2020-11-25 12:26:14 +0530

    script added

---
+++ DB/Insert_data.sql
@@ -0,0 +1,5 @@
+Insert into[dbo].[ITEM_DATA] ([ItemName],[Description],[Rate])
+Values('Item A','This is a item A description',499),
+('Item B','This is a item B description',49),
+('Item C','This is a item C description',1254),
+('Item D','This is a item D description',1000)
